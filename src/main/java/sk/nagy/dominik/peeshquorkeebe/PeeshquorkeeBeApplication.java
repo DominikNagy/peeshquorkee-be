5eba6351d98f8f6e66fd650eb9d0d366d1dd37a6
dominiknagy <dominiknagy12@gmail.com>
2021-01-14 21:40:16 +0100

    Starter project.

---
+++ src/main/java/sk/nagy/dominik/peeshquorkeebe/PeeshquorkeeBeApplication.java
@@ -0,0 +1,13 @@
+package sk.nagy.dominik.peeshquorkeebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeeshquorkeeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeeshquorkeeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/nagy/dominik/peeshquorkeebe/PeeshquorkeeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.nagy.dominik.peeshquorkeebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeeshquorkeeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
